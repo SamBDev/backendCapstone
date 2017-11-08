454c9448241b866c84703f7843438836a0aa05f9
Sam <samb283@gmail.com>
2017-11-08 09:17:16 -0600

    model update for join table

--- serverBackend/models/device.js
+++ serverBackend/models/device.js
@@ -1,13 +1,22 @@
 'use strict';
-module.exports = (sequelize, DataTypes) => {
+module.exports = function(sequelize, DataTypes) {
   var Device = sequelize.define(
     'Device',
-    { ipAddress: DataTypes.STRING, macAddress: DataTypes.STRING, deviceName: DataTypes.STRING },
-    { timestamps: true, paranoid: true }
+    {
+      ipAddress: DataTypes.STRING,
+      macAddress: DataTypes.STRING,
+      deviceName: DataTypes.STRING
+    },
+    {
+      timestamps: true,
+      paranoid: true
+    }
   );
 
   Device.associate = models => {
-    Device.belongsToMany(models.Device, { through: 'ScansDevices' });
+    Device.belongsToMany(models.Device, {
+      through: 'ScansDevices'
+    });
   };
   return Device;
 };
